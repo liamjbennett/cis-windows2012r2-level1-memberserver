9c9196104232dbd77bfb56f8a3d420343a3b0c4a
Steven Datt <steven.datt@gmail.com>
2018-06-02 08:11:56 +0100

    Audit Policy tests complete, firewall tests edit (#12)
    * fixing rules
    
    * completed firewall rules
    
    * firewall tests complete
    
    * removed level 2 rule 2.3.10.4
    
    * added utf-8 encoding tag for consistency
    
    * fixed regpath for rules
    
    * completed firewall rules
    
    * firewall tests complete
    
    * fixed regpath for rules
    
    * merge/rebase fix
    
    * audit policy tests complete

--- controls/windows_firewall.rb
+++ controls/windows_firewall.rb
@@ -7,7 +7,7 @@ control 'cis-domain-firewall-on-9.1.1' do
   title '9.1.1 Ensure Windows Firewall: Domain: Firewall state is set to On'
   desc 'Ensure Windows Firewall: Domain: Firewall state is set to On' 
   
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile') do
+  describe registry_key('DomainProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile') do
     its('EnableFirewall') { should eq 1 }
   end
 end
@@ -17,7 +17,7 @@ control 'cis-domain-firewall-inbound-9.1.2' do
   title '9.1.2 Ensure Windows Firewall: Domain: Inbound connections is set to Block'
   desc 'Ensure Windows Firewall: Domain: Inbound connections is set to Block'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile') do
+  describe registry_key('DomainProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile') do
     its('DefaultInboundAction') { should eq 1 }
   end
 end
@@ -27,7 +27,7 @@ control 'cis-domain-firewall-outbound-9.1.3' do
   title '9.1.3 Ensure Windows Firewall: Domain: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Domain: Outbound connections is set to Allow'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile') do
+  describe registry_key('DomainProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile') do
     its('DefaultOutboundAction') { should eq 0 }
   end
 end
@@ -37,7 +37,7 @@ control 'cis-domain-firewall-notification-9.1.4' do
   title '9.1.4 Ensure Windows Firewall: Domain: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Domain: Outbound connections is set to Allow'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile') do
+  describe registry_key('DomainProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile') do
     its('DisableNotifications') { should eq 1 }
   end
 end
@@ -73,7 +73,7 @@ control 'cis-domain-firewall-logging-name-9.1.7' do
   desc 'Ensure Windows Firewall: Domain: Logging: Name is set to
   %SYSTEMROOT%\System32\logfiles\firewall\domainfw.log'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile\Logging') do
+  describe registry_key('DomainProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging') do
     its('LogFilePath') { should eq '%SYSTEMROOT%\System32\logfiles\firewall\domainfw.log' }
   end
 end
@@ -85,7 +85,7 @@ control 'cis-domain-firewall-logging-size-limit-9.1.8' do
   desc 'Ensure Windows Firewall: Domain: Logging: Size limit (KB) is
   set to 16,384 KB or greater'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile\Logging') do
+  describe registry_key('DomainProfile', 'HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging') do
     its('LogFileSize') { should eq 16384 }
   end
 end
@@ -97,7 +97,7 @@ control 'cis-domain-firewall-log-dropped-packets-9.1.9' do
   desc 'Ensure Windows Firewall: Domain: Logging: Log dropped
   packets is set to Yes'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile\Logging') do
+  describe registry_key('DomainProfile', 'HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging') do
     its('LogDroppedPackets') { should eq 1 }
   end
 end
@@ -109,7 +109,7 @@ control 'cis-domain-firewall-log-successful-connections-9.1.10' do
   desc 'Ensure Windows Firewall: Domain: Logging: Log successful
   connections is set to Yes'
 
-  describe registry_key('DomainProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile\Logging') do
+  describe registry_key('DomainProfile', 'HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile\Logging') do
     its('LogSuccessfulConnections') { should eq 1 }
   end
 end
@@ -119,7 +119,7 @@ control 'cis-domain-firewall-on-9.2.1' do
   title '9.1.1 Ensure Windows Firewall: Private: Firewall state is set to On'
   desc 'Ensure Windows Firewall: Private: Firewall state is set to On' 
   
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile') do
     its('EnableFirewall') { should eq 1 }
   end
 end
@@ -129,7 +129,7 @@ control 'cis-private-firewall-inbound-9.2.2' do
   title '9.2.2 Ensure Windows Firewall: Private: Inbound connections is set to Block'
   desc 'Ensure Windows Firewall: Private: Inbound connections is set to Block'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile') do
     its('DefaultInboundAction') { should eq 1 }
   end
 end
@@ -139,7 +139,7 @@ control 'cis-private-firewall-outbound-9.2.3' do
   title '9.2.3 Ensure Windows Firewall: Private: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Private: Outbound connections is set to Allow'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile') do
     its('DefaultOutboundAction') { should eq 0 }
   end
 end
@@ -149,7 +149,7 @@ control 'cis-private-firewall-notification-9.2.4' do
   title '9.2.4 Ensure Windows Firewall: Private: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Private: Outbound connections is set to Allow'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile') do
     its('DisableNotifications') { should eq 1 }
   end
 end
@@ -185,7 +185,7 @@ control 'cis-private-firewall-logging-name-9.2.7' do
   desc 'Ensure Windows Firewall: Private: Logging: Name is set to
   %SYSTEMROOT%\System32\logfiles\firewall\privatefw.log'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile\Logging') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile\Logging') do
     its('LogFilePath') { should eq '%SYSTEMROOT%\System32\logfiles\firewall\privatefw.log' }
   end
 end
@@ -197,7 +197,7 @@ control 'cis-private-firewall-logging-size-limit-9.2.8' do
   desc 'Ensure Windows Firewall: Private: Logging: Size limit (KB) is
   set to 16,384 KB or greater'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile\Logging') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile\Logging') do
     its('LogFileSize') { should eq 16384 }
   end
 end
@@ -209,7 +209,7 @@ control 'cis-private-firewall-log-dropped-packets-9.2.9' do
   desc 'Ensure Windows Firewall: Private: Logging: Log dropped
   packets is set to Yes'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile\Logging') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile\Logging') do
     its('LogDroppedPackets') { should eq 1 }
   end
 end
@@ -221,7 +221,7 @@ control 'cis-private-firewall-log-successful-connections-9.2.10' do
   desc 'Ensure Windows Firewall: Private: Logging: Log successful
   connections is set to Yes'
 
-  describe registry_key('PrivateProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PrivateProfile\Logging') do
+  describe registry_key('PrivateProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PrivateProfile\Logging') do
     its('LogSuccessfulConnections') { should eq 1 }
   end
 end
@@ -231,7 +231,7 @@ control 'cis-domain-firewall-on-9.3.1' do
   title '9.3.1 Ensure Windows Firewall: Public: Firewall state is set to On'
   desc 'Ensure Windows Firewall: Public: Firewall state is set to On' 
   
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile') do
     its('EnableFirewall') { should eq 1 }
   end
 end
@@ -241,7 +241,7 @@ control 'cis-public-firewall-inbound-9.3.2' do
   title '9.3.2 Ensure Windows Firewall: Public: Inbound connections is set to Block'
   desc 'Ensure Windows Firewall: Public: Inbound connections is set to Block'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile') do
     its('DefaultInboundAction') { should eq 1 }
   end
 end
@@ -251,7 +251,7 @@ control 'cis-public-firewall-outbound-9.3.3' do
   title '9.3.3 Ensure Windows Firewall: Public: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Public: Outbound connections is set to Allow'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile') do
     its('DefaultOutboundAction') { should eq 0 }
   end
 end
@@ -261,7 +261,7 @@ control 'cis-public-firewall-notification-9.3.4' do
   title '9.3.4 Ensure Windows Firewall: Public: Outbound connections is set to Allow'
   desc 'Ensure Windows Firewall: Public: Outbound connections is set to Allow'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile') do
     its('DisableNotifications') { should eq 1 }
   end
 end
@@ -297,7 +297,7 @@ control 'cis-public-firewall-logging-name-9.3.7' do
   desc 'Ensure Windows Firewall: Public: Logging: Name is set to
   %SYSTEMROOT%\System32\logfiles\firewall\publicfw.log'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile\Logging') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging') do
     its('LogFilePath') { should eq '%SYSTEMROOT%\System32\logfiles\firewall\publicfw.log' }
   end
 end
@@ -309,7 +309,7 @@ control 'cis-public-firewall-logging-size-limit-9.3.8' do
   desc 'Ensure Windows Firewall: Public: Logging: Size limit (KB) is
   set to 16,384 KB or greater'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile\Logging') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging') do
     its('LogFileSize') { should eq 16384 }
   end
 end
@@ -321,7 +321,7 @@ control 'cis-public-firewall-log-dropped-packets-9.3.9' do
   desc 'Ensure Windows Firewall: Public: Logging: Log dropped
   packets is set to Yes'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile\Logging') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging') do
     its('LogDroppedPackets') { should eq 1 }
   end
 end
@@ -333,7 +333,8 @@ control 'cis-public-firewall-log-successful-connections-9.3.10' do
   desc 'Ensure Windows Firewall: Public: Logging: Log successful
   connections is set to Yes'
 
-  describe registry_key('PublicProfile', 'HKLM\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile\Logging') do
+  describe registry_key('PublicProfile', 'HKLM\Software\Policies\Microsoft\WindowsFirewall\PublicProfile\Logging') do
     its('LogSuccessfulConnections') { should eq 1 }
   end
 end
+
