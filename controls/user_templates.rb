c9aedef66216be10d4605b18e05236c8748a9cff
smdatt <steven.datt@gmail.com>
2018-06-18 17:15:31 +0100

    user templates complete

--- controls/user_templates.rb
+++ controls/user_templates.rb
@@ -169,7 +169,7 @@ control 'cis-do-not-preserve-zone-information-19.7.4.1' do
     end
 end
 
-control 'cis-do-not-preserve-zone-information-19.7.4.2' do
+control 'cis-notify-antivirus-programs-19.7.4.2' do
     impact 0.7
     title '19.7.4.2 Ensure Notify antivirus programs when opening attachments is set to Enabled'
     desc 'Ensure Notify antivirus programs when opening attachments is set to Enabled' 
@@ -180,13 +180,85 @@ control 'cis-do-not-preserve-zone-information-19.7.4.2' do
     describe registry_key({hive: 'HKEY_USERS'}).children(
         /S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]{3,}\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Attachments\z/).each { |key|
     describe registry_key(key) do
-        its('SaveZoneInformation') { should eq 0 }
+        its('ScanWithAntiVirus') { should eq 1 }
     end
     }
     describe registry_key('HKU\S-1-5-19\Software\Microsoft\Windows\CurrentVersion\Policies\Attachments') do  
-        its('SaveZoneInformation') { should eq 0 }
+        its('ScanWithAntiVirus') { should eq 1 }
     end
     describe registry_key('HKU\S-1-5-20\Software\Microsoft\Windows\CurrentVersion\Policies\Attachments') do  
-        its('SaveZoneInformation') { should eq 0 }
+        its('ScanWithAntiVirus') { should eq 1 }
+    end
+end
+
+# 19.7.25 Network Sharing
+
+control 'cis-prevent-profile-file-sharing-19.7.25.1' do
+    impact 0.7
+    title '19.7.25.1 Ensure Prevent users from sharing files within their profile is set to Enabled'
+    desc 'Ensure Prevent users from sharing files within their profile is set to Enabled' 
+
+    tag cis: ['windows_2012r2:19.7.25.1']
+    ref 'CIS Microsoft Windows Server 2012 R2 Benchmark'
+
+    describe registry_key({hive: 'HKEY_USERS'}).children(
+        /S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]{3,}\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\z/).each { |key|
+    describe registry_key(key) do
+        its('NoInplaceSharing') { should eq 1 }
+    end
+    }
+    describe registry_key('HKU\S-1-5-19\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer') do  
+        its('NoInplaceSharing') { should eq 1 }
+    end
+    describe registry_key('HKU\S-1-5-20\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer') do  
+        its('NoInplaceSharing') { should eq 1 }
+    end
+end
+
+# 19.7.37 Windows Installer
+
+control 'cis-always-elevated-install-19.7.37.1' do
+    impact 0.7
+    title '19.7.37.1 Ensure Always install with elevated privileges is set to Disabled'
+    desc 'Ensure Always install with elevated privileges is set to Disabled' 
+
+    tag cis: ['windows_2012r2:19.7.37.1']
+    ref 'CIS Microsoft Windows Server 2012 R2 Benchmark'
+
+    describe registry_key({hive: 'HKEY_USERS'}).children(
+        /S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]{3,}\\Software\\Policies\\Microsoft\\Windows\\Installer\z/).each { |key|
+    describe registry_key(key) do
+        its('AlwaysInstallElevated') { should eq 0 }
+    end
+    }
+    describe registry_key('HKU\S-1-5-19\Software\Policies\Microsoft\Windows\Installer') do  
+        its('AlwaysInstallElevated') { should eq 0 }
+    end
+    describe registry_key('HKU\S-1-5-20\Software\Policies\Microsoft\Windows\Installer') do  
+        its('AlwaysInstallElevated') { should eq 0 }
+    end
+end
+
+# 19.7.41.2 Playback
+
+control 'cis-always-elevated-install-19.7.41.2.1' do
+    impact 0.7
+    title '19.7.41.2.1 Ensure Prevent Codec Download is set to Enabled'
+    desc 'Ensure Prevent Codec Download is set to Enabled' 
+
+    tag cis: ['windows_2012r2:19.7.41.2.1']
+    ref 'CIS Microsoft Windows Server 2012 R2 Benchmark'
+
+    describe registry_key({hive: 'HKEY_USERS'}).children(
+        /S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]{3,}\\Software\\Policies\\Microsoft\\WindowsMediaPlayer\z/).each { |key|
+    describe registry_key(key) do
+        its('PreventCodecDownload') { should eq 1 }
+    end
+    }
+    describe registry_key('HKU\S-1-5-19\Software\Policies\Microsoft\WindowsMediaPlayer') do  
+        its('PreventCodecDownload') { should eq 1 }
+    end
+    describe registry_key('HKU\S-1-5-20\Software\Policies\Microsoft\WindowsMediaPlayer') do  
+        its('PreventCodecDownload') { should eq 1 }
     end
 end
